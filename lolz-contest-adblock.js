b04b2990668f94c00d1b98d6297bfa374a2f283d
InfiniteC0re <inf.core0@gmail.com>
2022-08-30 12:13:21 +0500

    Lolz Contest AdBlock v0.1

---
+++ lolz-contest-adblock.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Lolz Contest AdBlock
+// @version      0.1
+// @description  Скрывает контент постов с розыгрышами
+// @author       InfiniteC0re
+// @homepage     https://github.com/InfiniteC0re/TamperMonkey-Scripts
+// @downloadURL  https://github.com/InfiniteC0re/TamperMonkey-Scripts/master/lolz-contest-adblock.js
+// @updateURL    https://github.com/InfiniteC0re/TamperMonkey-Scripts/master/lolz-contest-adblock.js
+// @match        https://lolz.guru/threads/*
+// @icon         https://lolz.guru/favicon.svg
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    let contentBlock = document.querySelector(".messageText.SelectQuoteContainer.baseHtml.ugc");
+    let contestBlock = document.querySelector(".contestThreadBlock");
+
+    if (contestBlock) {
+        contentBlock.style.display = "none";
+    }
+})();
