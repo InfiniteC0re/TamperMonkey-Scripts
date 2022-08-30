8ae0295ea8b2852641775dfef37358e487d94ed7
InfiniteC0re <41500931+InfiniteC0re@users.noreply.github.com>
2022-08-30 18:29:32 +0500

    added zelenka.guru domain

--- lolz-contest-adblock.js
+++ lolz-contest-adblock.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Lolz Contest AdBlock
-// @version      0.2
+// @version      0.3
 // @description  Скрывает контент постов с розыгрышами
 // @author       InfiniteC0re
 // @homepage     https://github.com/InfiniteC0re/TamperMonkey-Scripts
 // @downloadURL  https://raw.githubusercontent.com/InfiniteC0re/TamperMonkey-Scripts/main/lolz-contest-adblock.js
 // @updateURL    https://raw.githubusercontent.com/InfiniteC0re/TamperMonkey-Scripts/main/lolz-contest-adblock.js
 // @match        https://lolz.guru/threads/*
+// @match        https://zelenka.guru/threads/*
 // @icon         https://lolz.guru/favicon.svg
 // @grant        none
 // ==/UserScript==
